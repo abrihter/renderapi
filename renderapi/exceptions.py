eee3c728cb05ceb4309e4e1cfeb3a61aa37467fa
abrihter <bojan.markovic.freelance@gmail.com>
2021-11-25 10:35:10 +0100

    Added service type exception

--- renderapi/exceptions.py
+++ renderapi/exceptions.py
@@ -5,3 +5,7 @@ custom exceptions
 class InvalidKeyException(Exception):
     '''invalid key exception'''
     pass
+
+class ServiceTypeException(Exception):
+    '''invalid service type exception'''
+    pass
